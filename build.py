c2a1baaf1023395d20238fdd459ac320ece2d84a
cambatch <cameron.batchelor22@gmail.com>
2024-07-02 21:32:43 -0700

    Update build

--- build.py
+++ build.py
@@ -2,21 +2,34 @@ import subprocess
 import sys
 import os
 
-def cmake_build(src_dir, build_dir, build_type):
+def cmake_build(build_dir, build_type, generator):
     curr_dir = os.getcwd()
 
     if not os.path.exists(build_dir):
         os.mkdir(build_dir)
 
+    os.chdir(build_dir)
+
+    if sys.platform.startswith('win32'):
+        if generator == 'Ninja':
+            cmake_cmd = ['cmake', '-S', '..', '-B', build_type, '-G', 'Ninja']
+            build_cmd = ['cmake', '--build', build_type]
+        elif generator == 'Visual Studio':
+            cmake_cmd = ['cmake', '..', '-G', 'Visual Studio 17 2022']
+            build_cmd = ['cmake', '--build', '.', '--config', build_type]
+        else:
+            print("Invalid generator for the windows platform")
+            return
+    elif sys.platform.startswith('linux'):
+        cmake_cmd = ['cmake', '-S', '..', '-B', build_type, '-G', 'Unix Makefiles']
+        build_cmd = ['cmake', '--build', build_type]
+    else:
+        print("Unsupported platform")
+        return
 
     try:
-        cmake_cmd = ['cmake', src_dir, '-B', build_dir, '-DCMAKE_C_COMPILER=clang', '-DCMAKE_CXX_COMPILER=clang++']
         subprocess.check_call(cmake_cmd)
-
-        os.chdir(build_dir)
-        build_cmd = ['cmake', '--build', '.', '--config', build_type]
         subprocess.check_call(build_cmd)
-
     except subprocess.CalledProcessError as e:
         print(f"Error occured: {e}")
     finally:
@@ -29,7 +42,7 @@ if __name__ == '__main__':
         exit()
 
     build_type = sys.argv[1]
-    src_dir = '.'
-    build_dir = 'bin/' + build_type
+    build_dir = 'bin/'
+    generator = 'Ninja'
 
-    cmake_build(src_dir, build_dir, build_type)
+    cmake_build(build_dir, build_type, generator)
